6eee2c9856e4f8308e5cb7c082abaaddfb433207
Jose G Gomez <127319809+T0546@users.noreply.github.com>
2023-08-28 19:20:21 -0300

    CrudGraphQL creado

---
+++ src/main/java/com/GjrsSoftware/core/GraphQlCrudApplication.java
@@ -0,0 +1,13 @@
+package com.GjrsSoftware.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQlCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphQlCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GjrsSoftware/core/GraphQlCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GjrsSoftware.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphQlCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
